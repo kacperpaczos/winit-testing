00a0267bd9fdc3500358994d40e48abb31a348cf
Kacper Paczos <kacper-paczos@linux.pl>
2024-02-23 12:02:59 +0100

    Init winit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Wait);
+    let window = Window::new(&event_loop).unwrap();
+
+    event_loop.run(move |event, elwt| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => elwt.exit(),
+            _ => (),
+        }
+    });
 }
